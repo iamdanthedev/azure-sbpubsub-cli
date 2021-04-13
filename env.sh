91b36789c7da6356db27cc15aa13496bd6c6077d
danthedev <iamdanthedev@gmail.com>
2021-04-13 12:37:05 +0300

    remove ignored files

--- env.sh
+++
@@ -1,7 +0,0 @@
-#SB_CONNECTION_STRING=Endpoint=sb://bonliva-service-bus-test.servicebus.windows.net/;SharedAccessKeyName=dev;SharedAccessKey=rz73fyATU9J2mekPJdWGJLnimjGjxnGBDhKtcLu0Wnk=
-#SB_TOPIC=danthedev
-
-#SB_CONNECTION_STRING=Endpoint=sb://bonliva-sb-danthedev.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=krI6UXMXyMf6oZSIJV7t728qYDITzgCFtcTZhWe60sY=
-export SB_CONNECTION_STRING="Endpoint=sb://bonliva-sb-dev.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=1+Yb+hyk76vXzZC00Q3iY3cYN3dPqKT1alDSWdjCKHk="
-export SB_TOPIC=main
-export SB_SUB=danthedev
